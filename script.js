7fb2a15668ef218736d567e0aa912d151c24ba06
Dante Hawler <202559257+D-Hawler@users.noreply.github.com>
2025-05-03 21:52:10 +0300

    final build
    Improved interface and functionality

--- script.js
+++ script.js
@@ -1,4 +1,40 @@
-let arrBook = [];
+let arrBook = [
+    {
+        "title": "Dune",
+        "author": "Frank Herbert",
+        "pages": "412",
+        "read": "Didn't read",
+        "idBook": "49a775eb-8016-4e07-9a0b-361429132052"
+    },
+    {
+        "title": "1984",
+        "author": "George Orwell",
+        "pages": "328",
+        "read": "Didn't read",
+        "idBook": "d3d0fae3-40f3-4b52-b76c-20f3e625f13a"
+    },
+    {
+        "title": "Sula",
+        "author": "Toni Morrison",
+        "pages": "192",
+        "read": "Didn't read",
+        "idBook": "2d56ddaf-9210-4d3a-8328-c5981c2f9349"
+    },
+    {
+        "title": "Coraline",
+        "author": "Neil Gaiman",
+        "pages": "194",
+        "read": "Didn't read",
+        "idBook": "ac2ec106-8bf6-4d52-8c7b-186079279d88"
+    },
+    {
+        "title": "Amarimonogatari",
+        "author": "Nisio isin",
+        "pages": "401",
+        "read": "Read",
+        "idBook": "72de8382-afe8-47d1-8495-4b7ceefea53c"
+    }
+];
 
 function CreatingBooks(title, author, pages, read, idBook) {
     if (!new.target) {
@@ -18,6 +54,11 @@ CreatingBooks.prototype.infoBook = function() {
 };
 
 
+const infoButton = document.querySelector("#infoButton");
+
+infoButton.addEventListener("click", function() {
+    console.log(arrBook);
+});
 
 
 const addBook = document.getElementById("addBook");
@@ -30,12 +71,23 @@ addBook.addEventListener("submit", function(event) {
     const pages = document.getElementById("pages").value;
     const read = document.getElementById("read").value;
 
-    let nameBook = title;
     nameBook = new CreatingBooks(title, author, pages, read, crypto.randomUUID());
 
     arrBook.push(nameBook);
 
     addBookToLibrary(nameBook);
+    
+
+
+    const titleInput = document.getElementById("title");
+    const authorInput = document.getElementById("author");
+    const pagesInput = document.getElementById("pages");
+    const readInput = document.getElementById("read");
+
+    titleInput.value = "";
+    authorInput.value = "";
+    pagesInput.value = "";
+    readInput.value = "Didn't read";
 });
 
 function addBookToLibrary(nameBook) {
@@ -113,6 +165,5 @@ library.addEventListener("click", function(event) {
         parentContainer.remove();
 
         arrBook = arrBook.filter(searchID => searchID.idBook !== parentContainer.id);
-        console.log(arrBook);
     };
 });
